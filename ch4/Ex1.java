e3f951dd93f97d2a0f044e7cf2d7c3a7614b5316
chaoxu1990 <xc900411@gmail.com>
2014-05-10 20:44:27 -0700

    Ch3 Ex1

---
+++ ch4/Ex1.java
@@ -0,0 +1,15 @@
+//Ex1
+
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+public class Ex1{
+	public static void main(String[] args){
+		print("Hello, it's:");
+		print(new Date());
+		
+		System.out.println("\nHello, it's");
+		System.out.println(new Date());
+	}
+}
+
