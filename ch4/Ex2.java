14b34b5f3050a9f9014a63c891130407dda13f68
chaoxu1990 <xc900411@gmail.com>
2014-05-10 21:12:10 -0700

    correct Ex2 and create Ex3

--- ch4/Ex2.java
+++ ch4/Ex2.java
@@ -1,19 +1,27 @@
-//Ex3
+//Ex2
 
 class Float{
 float f;
 }
 
-public class Ex2{
-	static void ali(Float y){
-		y.f = 10.0f;
-	}
-	
+public class Ex2{	
 	public static void main(String[] args){
-		Float x = new Float();
-		x.f = 100.0f;
-		System.out.println("x.f: "+x.f);
-		ali(x);
-		System.out.println("x.f: "+x.f);
+		Float x1 = new Float();
+		Float x2 = new Float();
+		
+		x1.f = 10.0f;
+		x2.f = 100.0f;
+		System.out.println("x1.f: "+x1.f);
+		System.out.println("x2.f: "+x2.f);
+
+		x1 = x2;
+		System.out.println("x1.f: "+x1.f);
+		System.out.println("x2.f: "+x2.f);
+
+		x2.f = 5.0f;
+		
+		System.out.println("x1.f: "+x1.f);
+		System.out.println("x2.f: "+x2.f);
+	
 	}
 }
---
+++ ch4/Ex3.java
@@ -0,0 +1,19 @@
+//Ex3
+
+class Float{
+float f;
+}
+
+public class Ex3{
+	static void ali(Float y){
+		y.f = 10.0f;
+	}
+	
+	public static void main(String[] args){
+		Float x = new Float();
+		x.f = 100.0f;
+		System.out.println("x.f: "+x.f);
+		ali(x);
+		System.out.println("x.f: "+x.f);
+	}
+}
