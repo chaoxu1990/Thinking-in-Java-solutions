9d92ea393f91b1108e2997d091993e7bbec12c49
chaoxu1990 <xc900411@gmail.com>
2014-05-19 16:20:43 -0700

    Create Ex9

---
+++ ch4/Ex9.java
@@ -0,0 +1,14 @@
+//Ex9
+import java.lang.*;
+public class Ex9 {
+	public static void main(String[] args) {
+		double max = Double.MAX_VALUE;
+		System.out.println("Max double = " + max);
+		double min = Double.MIN_VALUE; 
+		System.out.println("Min double = " + min);
+		float maxf = Float.MAX_VALUE;
+		System.out.println("Max float = " + maxf);
+		float minf = Float.MIN_VALUE; 
+		System.out.println("Min float = " + minf);	
+	}
+}
