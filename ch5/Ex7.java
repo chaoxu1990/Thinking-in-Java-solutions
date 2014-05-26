071abce98653865c2497ab8771ad4816e1a35156
chaoxu1990 <xc900411@gmail.com>
2014-05-26 14:47:34 -0700

    Create Ex7

---
+++ ch5/Ex7.java
@@ -0,0 +1,15 @@
+//Ex7
+
+import java.util.*;
+
+public class Ex7{
+	public static void main(String[] args){
+		for(int i = 1; i<=100; i++){
+			if(i==99) break;
+			//if(i==99) return;
+			System.out.println(i);
+		}
+		System.out.println("break");
+	}
+}
+
