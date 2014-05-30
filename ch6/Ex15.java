ed7477ba2d5035e93abd5208de6f731e57581d3b
chaoxu1990 <xc900411@gmail.com>
2014-05-30 14:44:20 -0700

    Create Ex15

---
+++ ch6/Ex15.java
@@ -0,0 +1,17 @@
+class Cup {
+
+	{
+		String s = "Instance Initialization";
+		System.out.println(s);
+
+		}
+}
+
+
+public class Ex15 {
+	public static void main(String[] args) {
+		System.out.println("Inside main()");
+		
+		Cup c = new Cup();
+	}
+}
