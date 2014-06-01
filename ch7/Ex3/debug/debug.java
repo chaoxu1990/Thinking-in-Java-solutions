a53aa69380b4ddda3259a45ac4ac2b6a1ddc4a98
chaoxu1990 <xc900411@gmail.com>
2014-05-31 23:47:44 -0700

    debug

---
+++ ch7/Ex3/debug/debug.java
@@ -0,0 +1,8 @@
+//debug.java
+package Ex2.debug;
+
+public class debug {
+	public static void debug(String s) {
+		System.out.println(s);
+	}
+}
---
+++ ch7/Ex3/debugoff/debug.java
@@ -0,0 +1,7 @@
+//debug.java
+package Ex2.debugoff;
+public class debug {
+	public static void debug(String s) {
+		System.out.println(s);
+	}
+}
