7a6c5ba4e2b4f41e37d5049a2063042383cda1b2
chaoxu1990 <xc900411@gmail.com>
2014-05-31 21:40:01 -0700

    Create Ex1

---
+++ ch7/Ex1/first.java
@@ -0,0 +1,11 @@
+//Ex1
+
+//import newpackage.*;
+public class first {
+	public static void main(String[] args) {
+		System.out.println("first!");
+		String[] a = {"aaa","bbb"};
+		newpackage.second s = new newpackage.second();
+		s.main(a);
+	}	
+}
---
+++ ch7/Ex1/newpackage/second.java
@@ -0,0 +1,10 @@
+//Ex1
+package newpackage;
+
+public class second {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("second!");
+	}	
+}
