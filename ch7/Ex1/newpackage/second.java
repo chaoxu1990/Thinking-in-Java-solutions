ec0cbd1d05c47b92aeb8c55122423b267c39d7f6
chaoxu1990 <xc900411@gmail.com>
2014-05-31 21:52:58 -0700

    commit

--- ch7/Ex1/newpackage/second.java
+++ ch7/Ex1/newpackage/second.java
@@ -3,8 +3,6 @@ package newpackage;
 
 public class second {
 	public static void main(String[] args) {
-		
-		
 		System.out.println("second!");
 	}	
 }
---
+++ ch7/Ex2.java
@@ -0,0 +1,21 @@
+// access/Collision.java
+// TIJ4 Chapter Access, Exercise 2, page 217
+/* Take the code fragments in this section and turn them into a program and 
+* verify that collisions do occur.
+*/
+
+/*	This code is copied from http://greggordon.org/java/tij4/access/Collision.java
+*	Because I didn't download the source code package from mindview.net 
+*/
+
+
+import net.mindview.simple.*;
+import java.util.*;
+ 
+public class Collision {
+	public static void main(String[] args) {
+		// Vector v = new Vector(); // ambiquous collision
+		net.mindview.simple.Vector v1 = new net.mindview.simple.Vector();
+		java.util.Vector v2 = new java.util.Vector();
+	}
+}
