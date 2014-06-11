ed33c07754b2574a92dec2e0606d29e79a91a4e6
chaoxu1990 <xc900411@gmail.com>
2014-06-11 14:31:08 -0700

    Create Ex7

---
+++ ch7/Ex7/Access/Widget.java
@@ -0,0 +1,7 @@
+//Widget.java
+
+package Access;
+
+public class Widget{
+		public Widget(){System.out.println("Hello, Widget!");}
+}
---
+++ ch7/Ex7/MakeWidget.java
@@ -0,0 +1,10 @@
+//makeWidget.java
+
+import Access.*;
+
+public class MakeWidget{
+		public static void main(String [] args){
+			Widget w = new Widget();
+			System.out.println("This is widget!");
+		}
+}
